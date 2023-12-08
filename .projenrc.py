3a3a0e353f6f0537f3b1af7718bd0e918324f79d
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-08 12:49:31 -0800

    chore: project created with projen

---
+++ .projenrc.py
@@ -0,0 +1,12 @@
+from projen.awscdk import AwsCdkPythonApp
+
+project = AwsCdkPythonApp(
+    author_email="ernest@quiltdata.io",
+    author_name="Dr. Ernie Prabhakar",
+    cdk_version="2.1.0",
+    module_name="sentinel_packager",
+    name="sentinel-packager",
+    version="0.1.0",
+)
+
+project.synth()
