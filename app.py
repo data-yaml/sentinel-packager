5e738bbc8a9e696d511351a17da5685ade4da9cb
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-09 15:11:19 -0800

    SentinelPackagerStack

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
 from aws_cdk import App, Environment
-from sentinel_packager.main import MyStack
+from sentinel_packager.main import SentinelPackagerStack
 
 # for development, use account/region from cdk cli
 dev_env = Environment(
-  account=os.getenv('CDK_DEFAULT_ACCOUNT'),
-  region=os.getenv('CDK_DEFAULT_REGION')
+    account=os.getenv("CDK_DEFAULT_ACCOUNT"),
+    region=os.getenv("CDK_DEFAULT_REGION")
 )
 
 app = App()
-MyStack(app, "sentinel-packager-dev", env=dev_env)
-# MyStack(app, "sentinel-packager-prod", env=prod_env)
+SentinelPackagerStack(app, "sentinel-packager-dev", env=dev_env)
+# SentinelPackagerStack(app, "sentinel-packager-prod", env=prod_env)
 
 app.synth()
